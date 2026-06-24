eb9a524f977c3540a5937b5c312edda5b5f314f9
m301her-prog <m301her@gmail.com>
2026-06-24 06:03:56 +0300

    Update base path comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // هذا السطر هو الحل الجوهري للمشكلة في كثير من الحالات
+  base: './' // هذا هو السطر المهم جداً لحل الصفحة البيضاء
 })
