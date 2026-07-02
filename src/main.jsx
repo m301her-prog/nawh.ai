f8fc269e56a7b6e23a788dd0ca23b27f991e6131
m301her-prog <m301her@gmail.com>
2026-07-02 15:25:11 +0300

    Added .gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// إضافة فحص بسيط للتأكد من وجود العنصر قبل محاولة الرندرة
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("خطأ: لم يتم العثور على عنصر 'root' في صفحة الـ HTML!");
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
